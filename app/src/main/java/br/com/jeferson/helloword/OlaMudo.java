22402a3dcaa965d9a34cd721d5a90c8af943a565
Jeferson Ayrles <gb.bacabal.jeferson@gmail.com>
2016-11-04 01:00:28 -0200

    Initial commit

---
+++ app/src/main/java/br/com/jeferson/helloword/OlaMudo.java
@@ -0,0 +1,13 @@
+package br.com.jeferson.helloword;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OlaMudo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ola_mudo);
+    }
+}
